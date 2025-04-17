52c09c18ac2df30bc17b896b2fa7ee3a510c0e16
you <you@example.com>
2025-04-17 18:12:11 +0200

    Direkt hiba

--- hello.py
+++ hello.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/api/hello")
 def hello():
-    return jsonify({"message": "hello"})
+    return jsonify({"message": "hello2"})
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
