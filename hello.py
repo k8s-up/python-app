667a95be0bc0906ecbf5562e7a2f13fdc81d37f8
you <you@example.com>
2025-04-17 19:01:32 +0200

    Átnyúlás a másik repo-ba

--- hello.py
+++ hello.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/api/hello")
 def hello():
-    return jsonify({"message": "hello2"})
+    return jsonify({"message": "hello ArgoCD"})
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
