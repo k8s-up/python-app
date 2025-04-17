20e1f5ac8aa40a4404555bd49c6ef6b19144ee8e
you <you@example.com>
2025-04-17 19:02:31 +0200

    Tesztest módosítása

--- test_hello.py
+++ test_hello.py
@@ -13,4 +13,4 @@ def test_add(client):
     response = client.get("/api/hello")
     json_data = response.get_json()
     assert response.status_code == 200
-    assert json_data["message"] == "hello"
+    assert json_data["message"] == "hello ArgoCD"
